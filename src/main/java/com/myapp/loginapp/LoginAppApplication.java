50fc0575a62c92baed0378bd04d27799a67e1caf
Dh3kon <kb-max@hotmail.com>
2020-05-27 15:58:43 -0300

    Application properties and Main class updated

--- src/main/java/com/myapp/loginapp/LoginAppApplication.java
+++ src/main/java/com/myapp/loginapp/LoginAppApplication.java
@@ -2,12 +2,26 @@ package com.myapp.loginapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = {
+        LoginAppApplication.class,
+        Jsr310JpaConverters.class
+})
 public class LoginAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginAppApplication.class, args);
-	}
+    @PostConstruct
+    void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAppApplication.class, args);
+    }
 
 }
